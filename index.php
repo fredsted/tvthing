626ec07b32371e196b465dc77beaba13095d0dc8
Simon Fredsted <simon@fairweb.dk>
2015-01-05 10:53:00 +0100

    Add iDevice touch icon + favicon

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <title>TV</title>
     <link rel="stylesheet" href="css/style.css"/>
+    <link rel="shortcut icon" href="img/iconified/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="img/iconified/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/iconified/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/iconified/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/iconified/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/iconified/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="img/iconified/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="img/iconified/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="img/iconified/apple-touch-icon-152x152.png" />
 </head>
 <script src="js/vendor/jquery.js"></script>
 <script>
