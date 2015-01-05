220c3996d2f79db32692cddc9395e77a865126e8
Simon Fredsted <simon@fairweb.dk>
2015-01-05 10:56:05 +0100

    Add iDevice touch icon

--- config.php
+++ config.php
@@ -12,15 +12,15 @@
 
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" href="/iconified/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="/iconified/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/iconified/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/iconified/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/iconified/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/iconified/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/iconified/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/iconified/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/iconified/apple-touch-icon-152x152.png" />
+    <link rel="shortcut icon" href="img/iconified/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="img/iconified/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/iconified/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/iconified/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/iconified/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/iconified/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="img/iconified/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="img/iconified/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="img/iconified/apple-touch-icon-152x152.png" />
 
     <script src="js/danmark.js"></script>
     <script src="js/vendor/custom.modernizr.js"></script>
